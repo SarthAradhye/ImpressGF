edff5f528c8d3148e788460f4ae8b0d84eb44dee
SarthAradhye <86707040+SarthAradhye@users.noreply.github.com>
2023-10-14 13:20:50 +0530

    Added files through upload
    adding files

---
+++ Script.js
@@ -0,0 +1,14 @@
+function playMusicA(){
+    var music = new Audio('Tum Kab Aaoge_64(PagalWorld.com.pe).mp3');
+    music.play();
+    }
+function playMusic(){
+    var music = new Audio('_Heeriye_64(PagalWorld.com.pe).mp3');
+    music.play();
+    }
+function Alert(){
+    alert("I will be always there with you 🤗❤️")
+}
+function Alert_A(){
+    alert("Babes I always love you don't leave me 🥹😭")
+} 
